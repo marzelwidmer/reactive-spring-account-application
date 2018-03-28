6479e8208bcac1d9aa86f5de6e0a782561c7febc
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-28 07:07:55 +0200

    remove ReactiveCrudRepository own impl

--- src/main/java/ch/keepcalm/react/spring/ReactiveAccountRepository.java
+++ src/main/java/ch/keepcalm/react/spring/ReactiveAccountRepository.java
@@ -1,5 +1,6 @@
 package ch.keepcalm.react.spring;
 
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
